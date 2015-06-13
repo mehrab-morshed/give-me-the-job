ba88ef975c3e6348e7030a69f7dc699bd84a36de
pathfinder <mehrab.morshed@gmail.com>
2015-06-14 03:10:58 +0600

    fourth

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,7 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		//$this->load->view('welcome_message');
 		
 		$query = $this->db->get("employees");
 		foreach($query->result()) as $row)
