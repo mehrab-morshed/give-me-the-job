c0175728718a45a33b61ef635c6e034240129056
pathfinder <mehrab.morshed@gmail.com>
2015-06-15 13:33:44 +0600

    8

---
+++ application/controllers/Greetings.php
@@ -0,0 +1,22 @@
+<?php
+
+require(APPPATH . '/libraries/REST_Controller.php');
+
+class Greetings extends REST_Controller {
+
+    function index() {
+        $this->response(array('name' => 'magi'));
+    }
+
+    function answer_get() {
+        if (!$this->get('q')) {
+            $this->response(NULL, 400);
+        }
+		$q = rawurldecode($q);
+		$this->response($q);
+        //$this->db->where('employee_id', $this->get('id'));
+		
+        
+    }
+}
+
