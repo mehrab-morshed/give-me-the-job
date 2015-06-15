c18a4b27d84d91106959c40916c60bea719bd687
pathfinder <mehrab.morshed@gmail.com>
2015-06-15 13:56:05 +0600

    11

--- application/controllers/Greetings.php
+++ application/controllers/Greetings.php
@@ -5,16 +5,12 @@ require(APPPATH . '/libraries/REST_Controller.php');
 class Greetings extends REST_Controller {
 
     function index() {
-        $this->response(array('name' => 'magi'));
-    }
-
-    function index_get() {
         if (!$this->get('q')) {
             $this->response(NULL, 400);
         }
 		echo $this->get('q');
 		
-		if((strpos($q,"Hi!")!=false) || strpos($q,"Hello")!=false))
+		if(strpos($q,"Hi!")!=false || strpos($q,"Hello")!=false)
 			$data['response'] = "Hello, Kitty! Welcome to planet Earth!"
 		else 
 			$data['response'] = "invalid question";
