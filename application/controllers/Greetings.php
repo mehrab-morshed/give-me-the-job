3b7d4231c4f86934f6d1aaad2622cc62d4eb8b29
pathfinder <mehrab.morshed@gmail.com>
2015-06-15 14:21:23 +0600

    14

--- application/controllers/Greetings.php
+++ application/controllers/Greetings.php
@@ -1,24 +1,24 @@
 <?php
 
-//require(APPPATH . '/libraries/REST_Controller.php');
-
 class Greetings extends CI_Controller {
 
     function index() {
-		if(isset($_GET["q"])){
-		
-			//if (!$this->get('q')) {
-		//		$this->response(NULL, 400);
-		//	}
-			echo $_GET["q"];
-			$q = $_GET["q"];
-			if(strpos($q,"Hi!")!==false || strpos($q,"Hello")!==false)
-				$data['answer'] = "Hello, Kitty! Welcome to planet Earth!";
-			else 
-				$data['answer'] = "invalid question";
-			
-			$this->response($data);
-    }
-	else echo "please give a question";
-}
+        if (($_GET['q'])) {
 
+            $data = array();
+            $q = $_GET['q'];
+
+            if ((strpos($q, 'Hi!') !== false) || (strpos($q, 'Hello') !== false) || (strpos($q, 'Good morning!') !== false) || (strpos($q, 'Good evening!') !== false) || (strpos($q, 'Good night!') !== false)) {
+
+                $data['answer'] = 'Hello, Kitty! I am busy with screening test';
+            } else {
+                $data['answer'] = 'Invalid Question';
+            }
+            $result = json_encode($data);
+            echo $result;
+        } else {
+            echo show_404();
+        }
+    }
+
+}
