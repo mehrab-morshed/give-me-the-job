118b5bd35dc1bf0769aaebd384074aa1fed9c552
pathfinder <mehrab.morshed@gmail.com>
2015-06-16 03:06:40 +0600

    Greeting hello

--- application/controllers/Greetings.php
+++ application/controllers/Greetings.php
@@ -8,7 +8,7 @@ class Greetings extends CI_Controller {
             $data = array();
             $q = $_GET['q'];
 
-            if ((strpos($q, 'Hi!') !== false) || (strpos($q, 'Hello') !== false) || (strpos($q, 'Good morning!') !== false) || (strpos($q, 'Good evening!') !== false) || (strpos($q, 'Good night!') !== false)) {
+            if ((strpos($q, 'Hi!') !== false) || (strpos($q, 'Hello!') !== false) || (strpos($q, 'Good morning!') !== false) || (strpos($q, 'Good evening!') !== false) || (strpos($q, 'Good night!') !== false)) {
 
                 $data['answer'] = 'Hello, Kitty! I am busy with screening test';
             } else {
